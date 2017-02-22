f582456135dd127fef5ed9c4bbcba73819cb82b6
Magicfrost <250254501@qq.com>
2017-02-22 23:10:07 +0800

    加入创建数据库和表的各项配置.

---
+++ app/src/main/java/com/itheima/frostweather/db/Country.java
@@ -0,0 +1,46 @@
+package com.itheima.frostweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by song on 2017/2/22.
+ */
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private int cityId;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
